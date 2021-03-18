7569871161abe9b88ebfd55e2737e89041ad05b6
amangsingh <68422352+amangsingh@users.noreply.github.com>
2021-03-18 11:45:32 +0530

    Revert "final commit --challenge submission"
    This reverts commit ae7af7c7e87137ef4e329f4238bf7156520d8415.

--- lib/nav/appbar.dart
+++ lib/nav/appbar.dart
@@ -1,4 +1,3 @@
-import 'package:calc_challenge/screens/history.dart';
 import 'package:flutter/material.dart';
 
 class CustomNav extends StatelessWidget with PreferredSizeWidget {
@@ -6,21 +5,6 @@ class CustomNav extends StatelessWidget with PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       title: Text('Calculator'),
-      actions: [
-        Padding(
-          padding: const EdgeInsets.only(right: 25.0),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => history()));
-            },
-            child: Icon(
-              Icons.history,
-              size: 30,
-            ),
-          ),
-        )
-      ],
     );
   }
 
